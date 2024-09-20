415f3d7af04ff3a86311d122ecd51430f188c995
Hiddentale <55276630+Hiddentale@users.noreply.github.com>
2024-09-20 18:18:55 +0200

    Finished week3

--- numerical_methods_1.py
+++ numerical_methods_1.py
@@ -1,21 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
+# Ik neem aan dat de functie en variable namen grotendeels zichzelf documenten als ze goed
+# benaamd zijn, als je om een of andere reden overal comments wilt hebben geef dat aub aan.
+# Ik weet dat het gebruikelijk is in academische wetenschappen om grotendeels al je code
+# te commenten, maar dat is juist omdat de gemiddelde academische wetenschapper echt heel
+# slecht is in programmeren en dus wel in taal moet uitleggen wat hun onleesbare code doet.
 
 def plot_function(x_axis, y_axis, output_name):
     fig, ax = plt.subplots()
-    ax.set_xlabel('x', fontsize=14, fontweight='bold')
-    ax.set_ylabel('y', fontsize=14, fontweight='bold')
+    ax.set_title("The relation between the size of a matrix and its condition number")
+    ax.set_xlabel('size of matrix', fontsize=14, fontweight='bold')
+    ax.set_ylabel('condition number', fontsize=14, fontweight='bold')
 
     ax.plot(x_axis, y_axis)
     plt.savefig(output_name, dpi=150, bbox_inches='tight')
 
-def estimate_alpha(x_axis, y_axis):
+def estimate_quadratic_exponent(x_axis, y_axis):
     logarithmic_condition_numbers = [np.log(element) for element in y_axis]
     coefficients = np.polyfit(x_axis, logarithmic_condition_numbers, 1)
-    scaling_factor = np.exp(coefficients[0])
-    return scaling_factor
+    quadratic_exponent = np.exp(coefficients[0])
+    return quadratic_exponent
 
-def calculate_condition_numbers(matrix_sizes):
+def calculate_condition_numbers():
     condition_numbers = []
 
     for i in range(4, 13):
@@ -32,5 +38,5 @@ y_axis = calculate_condition_numbers()
 plot_function(x_axis, y_axis, "output.png")
 
 # Opdracht (b)
-scaling_factor = estimate_alpha(x_axis, y_axis)
-print(f"The estimated scaling factor alpha is: {scaling_factor}")
+quadratic_exponent = estimate_quadratic_exponent(x_axis, y_axis)
+print(f"The estimated scaling factor alpha is: {quadratic_exponent}")
