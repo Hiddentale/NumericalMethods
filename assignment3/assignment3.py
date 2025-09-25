2b71f66884e69cf278eb52a83f92b6e1149e2cdf
Hiddentale <55276630+Hiddentale@users.noreply.github.com>
2025-09-25 15:26:18 +0200

    Some progress

--- assignment3/assignment3.py
+++ assignment3/assignment3.py
@@ -2,46 +2,16 @@ import numpy as np
 from numpy.typing import NDArray
 
 
-def LU_decomposition_old(matrix: NDArray) -> NDArray:
-    """
-    Returns the LU decomposition of a given square matrix without using pivoting.
+def matrix_is_not_square(matrix: NDArray) -> bool:
+    return matrix.shape[0] != matrix.shape[1]
 
-    Arguments:
-        matrix: NDArray, the square matrix to be decomposed
-    """
 
-    if matrix.shape[0] != matrix.shape[1]:
-        raise TypeError("Given matrix is not square.")
+def matrix_is_not_of_type_float(matrix: NDArray) -> bool:
+    return matrix.dtype is not float
 
-    # Construct empty L and U matrices with same dimensions as given matrix
-    length_of_given_square_matrix = len(matrix)
-    matrix_shape = (length_of_given_square_matrix, length_of_given_square_matrix)
-    lower_triangular, upper_triangular = np.zeros(matrix_shape), np.zeros(matrix_shape)
 
-    # Calculate both L and U matrices
-    for i in range(length_of_given_square_matrix):
-        # Fill diagonal of L matrix with 1's
-        lower_triangular[i][i] = 1
-        for j in range(i, length_of_given_square_matrix):
-            sum_of_previous_calculated_elements = 0
-            for k in range(i):
-                sum_of_previous_calculated_elements += (
-                    lower_triangular[i][k] * upper_triangular[k][j]
-                )
-            upper_triangular[i][j] = matrix[i][j] - sum_of_previous_calculated_elements
-
-        for m in range(i + 1, length_of_given_square_matrix):
-            sum_of_previous_calculated_elements = 0
-            for k in range(i):
-                sum_of_previous_calculated_elements += (
-                    lower_triangular[m][k] * upper_triangular[k][i]
-                )
-            lower_triangular[m][i] = (
-                matrix[m][i] - sum_of_previous_calculated_elements
-            ) / upper_triangular[i][i]
-
-    return lower_triangular, upper_triangular
-
+def matrices_have_mismatched_sizes(matrix_length: int, right_hand_side: int) -> bool:
+    return len(right_hand_side) != matrix_length
 
 
 def LU_decomposition(matrix: NDArray) -> NDArray:
@@ -57,43 +27,31 @@ def LU_decomposition(matrix: NDArray) -> NDArray:
         matrix = matrix.astype(float)
 
     dictionary = {}
-    # Forward elimination
+    # Forward elimination, column by column
     for pivot_index in range(len(matrix)):
         # Eliminate entries below the current pivot
         for matrix_index in range(pivot_index + 1, len(matrix)):
-            multiplier = matrix[matrix_index, pivot_index] / matrix[pivot_index, pivot_index]
-            matrix[matrix_index] = matrix[matrix_index] - multiplier * matrix[pivot_index]
+            # Value we multiply the pivot by to eliminate the i-th entry of the row
+            multiplier = (
+                matrix[matrix_index, pivot_index] / matrix[pivot_index, pivot_index]
+            )
+            # Eliminate the i-th entry of the row
+            matrix[matrix_index] = (
+                matrix[matrix_index] - multiplier * matrix[pivot_index]
+            )
             # Save current lower triangular part for later usage
             dictionary[matrix_index, pivot_index] = multiplier
+
     # Add the lower part of the matrix
     for (row, col), multi in dictionary.items():
         matrix[row, col] = multi
     return matrix
 
 
-input = np.array([[6, 18, 3], [2, 12, 1], [4, 15, 3]])
-print(f"input:\n {input}\n")
-
-output = LU_decomposition(input)
-print(f"output: \n {output}\n")
-
-# _________________________________________________________________________________________________________________________
-
-def matrix_is_not_of_type_float(matrix: NDArray) -> bool:
-    return matrix.dtype is not float
-
-def matrix_is_not_square(matrix: NDArray) -> bool:
-    return matrix.shape[0] != matrix.shape[1]
-
-def matrices_have_mismatched_sizes(matrix_length, right_hand_side):
-    """"""
-    return len(right_hand_side) != matrix_length
-
-
 def forward_substitute_from_first_to_last_row(
     matrix_length, solution_vector, triangular_matrix, right_hand_side
 ):
-    """"""
+    """Applies the forward substitution algorithm to the given triangular matrix"""
     for i in range(matrix_length):
         solution_vector[i] = right_hand_side[i] - np.dot(
             triangular_matrix[i, :i], solution_vector[:i]
@@ -103,7 +61,7 @@ def forward_substitute_from_first_to_last_row(
 def forward_substitute_from_last_to_first_row(
     matrix_length, solution_vector, triangular_matrix, right_hand_side
 ):
-    """"""
+    """Applies the backward substitution algorithm to the given triangular matrix"""
     for i in range(matrix_length - 1, -1, -1):
         solution_vector[i] = (
             right_hand_side[i]
@@ -111,7 +69,7 @@ def forward_substitute_from_last_to_first_row(
         ) / triangular_matrix[i, i]
 
 
-def forward_solve(triangular_matrix, right_hand_side):
+def forward_solve(triangular_matrix: NDArray, right_hand_side: NDArray) -> NDArray:
     """Solve a lower triangular system"""
 
     matrix_length = len(triangular_matrix)
@@ -145,18 +103,31 @@ def backward_solve(triangular_matrix, right_hand_side):
     return solution_vector
 
 
-def LU_solve(matrix, right_hand_side):
-    """Solves the given linear system by using LU decomposition."""
-    lower_triangular, upper_triangular = LU_decomposition(matrix)
+def LU_solve(matrix: NDArray, right_hand_side: NDArray):
+    """Solves the given linear system by using LU decomposition.
+
+    Arguments:
+        matrix: NDArray, the square matrix to be solved
+        right_hand_side: NDArray,
+    """
+    lu_decomposed_matrix = LU_decomposition(matrix)
+    lower_triangular, upper_triangular = np.tril(lu_decomposed_matrix), np.triu(
+        lu_decomposed_matrix
+    )
     intermediate_vector = forward_solve(lower_triangular, right_hand_side)
+    print(intermediate_vector)
     solution = backward_solve(upper_triangular, intermediate_vector)
-    return solution
+    return solution, lu_decomposed_matrix
 
 
-# matrix = np.array([[6, 18, 3], [2, 12, 1], [4, 15, 3]])
+if __name__ == "__main__":
 
-# right_hand_side = np.array([3, 19, 0])
+    input = np.array([[6, 18, 3], [2, 12, 1], [4, 15, 3]])
+    print(f"input:\n {input}\n")
 
-# solution = LU_solve(matrix, right_hand_side)
-# print(solution)
-# print(np.dot(matrix, solution))
+    right_hand_side = np.array([3, 19, 0])
+
+    solution, lu_decomposed_matrix = LU_solve(input, right_hand_side)
+    print(f"solution: \n {solution}\n")
+    print(lu_decomposed_matrix)
+    print(np.dot(input, solution))
