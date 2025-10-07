2dccfdfa7ef982ffd668d150254ca2537bbebe0c
Johan Voskamp <55276630+Hiddentale@users.noreply.github.com>
2025-10-07 08:42:10 +0200

    Initial commit

---
+++ assignment5/assignment5.py
@@ -0,0 +1,16 @@
+def fixed_point(
+    given_function, initial_guess: float, tolerance, max_iterations_allowed
+):
+    """
+
+    Arguments:
+        given_function: , The function that we want to solve numerically with fixed point iteration
+        initial_guess: float32, initial guess found by plotting function
+        tolerance: float32,
+        max_iterations_allowed: int
+
+    Returns:
+    """
+    for i in max_iterations_allowed:
+        None
+    raise RuntimeError("Couldn't converge in specified amount of iterations")
