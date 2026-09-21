4c598d1ed9aacc892504eb4cb8367f66661cd92b
Johan Voskamp <55276630+Hiddentale@users.noreply.github.com>
2026-09-21 14:51:31 +0200

    chore: save

--- num_pde_3.py
+++ num_pde_3.py
@@ -53,16 +53,19 @@ if __name__ == "__main__":
         int((1 / space_step) - 1) for space_step in space_steps
     ]
 
-    x_values = space_steps[0] * np.arange(1, number_of_interior_points[0] + 1)
-    print(f"x_values: {x_values}")
-    number_of_time_steps = round(final_time / time_steps[0])
-    initial_values = initial_condition(x_values)
+    errors = []
+    for i in range(number_of_iterations):
+        x_values = space_steps[i] * np.arange(1, number_of_interior_points[i] + 1)
+        print(f"x_values: {x_values}")
+        number_of_time_steps = round(final_time / time_steps[i])
+        initial_values = initial_condition(x_values)
 
-    approximated_values = initial_values
-    tridiagonal = tridiagonal_S(number_of_interior_points[0], mesh_fourier_number)
-    for _ in range(number_of_time_steps):
-        approximated_values = tridiagonal @ approximated_values
-    print(f"approx_values: {approximated_values}")
-    difference = exact_solution(x_values, 0.2) - approximated_values
-    print(f"difference: {difference}")
-    print(np.max(difference))
+        approximated_values = initial_values
+        tridiagonal = tridiagonal_S(number_of_interior_points[i], mesh_fourier_number)
+        for _ in range(number_of_time_steps):
+            approximated_values = tridiagonal @ approximated_values
+        print(f"approx_values: {approximated_values}")
+        difference = abs(exact_solution(x_values, 0.2) - approximated_values)
+        print(f"difference: {difference}")
+        errors.append(np.max(difference))
+    print(errors)
