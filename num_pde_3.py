07576fe8e196ebbb23525145a326cd9d7a9054c7
Johan Voskamp <55276630+Hiddentale@users.noreply.github.com>
2026-09-22 13:33:39 +0200

    docs: added docstrings

--- num_pde_3.py
+++ num_pde_3.py
@@ -5,47 +5,47 @@ MESH_FOURIER_NUMBER = 1 / 6
 
 
 def approximate_solution(final_time, time_steps, x_values, number_of_interior_points):
-    """"""
+    """Approximates the heat equation through the use of a FCTS algorithm."""
     number_of_time_steps = round(final_time / time_steps[i])
     initial_values = initial_condition(x_values)
 
     approximated_values = initial_values
     tridiagonal = tridiagonal_S(number_of_interior_points[i])
     for _ in range(number_of_time_steps):
-        approximated_values = fcts_algorithm(tridiagonal, approximated_values)
+        approximated_values = ftcs_algorithm(tridiagonal, approximated_values)
     return approximated_values
 
 
 def calculate_error(x_values, approximated_values):
-    """"""
+    """Calculates the maximum error between components of two matrices."""
     difference = abs(exact_solution(x_values, 0.2) - approximated_values)
     return np.max(difference)
 
 
 def exact_solution(x, t):
-    """"""
+    """The given exact solution."""
     return np.pow(np.e, (-4 * np.pow(np.pi, 2) * t)) * np.sin(2 * np.pi * x)
 
 
-def fcts_algorithm(tridiagonal, approximated_values):
-    """"""
+def ftcs_algorithm(tridiagonal, approximated_values):
+    """The given matrix-based Forward Time Centered Space algorithm."""
     return approximated_values @ tridiagonal
 
 
 def initial_condition(x: np.ndarray):
-    """"""
+    """The given initial condition."""
     return np.sin(2 * (np.pi) * x)
 
 
 def plot_error(space_step_vector, error):
-    """"""
+    """Plots the given error on a loglog graph."""
     plt.loglog(space_step_vector, error)
     plt.grid()
     plt.show()
 
 
 def tridiagonal_S(matrix_width):
-    """"""
+    """Constructs a tridagonal matrix given a matrix width."""
     diagonal = np.full(shape=matrix_width, fill_value=1 - 2 * MESH_FOURIER_NUMBER)
     lower_diagonal = np.full(shape=matrix_width - 1, fill_value=MESH_FOURIER_NUMBER)
     tridiagonal = (
